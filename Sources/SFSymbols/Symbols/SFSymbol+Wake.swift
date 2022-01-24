afd445bdbb620bf36db60de8b169b29255934633
ggoraa <ggoraa1029@gmail.com>
2022-01-24 22:25:08 +0200

    feat: removed unneeded usages of UIKit in symbol declarations

--- Sources/SFSymbols/Symbols/SFSymbol+Wake.swift
+++ Sources/SFSymbols/Symbols/SFSymbol+Wake.swift
@@ -19,8 +19,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import UIKit
-
 extension SFSymbol {
 
 	public static var wake: Wake { .init(name: "wake") }
